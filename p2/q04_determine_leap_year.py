b0057da906f19df41b728443f1156f6a40a016c8
AliceShi <shi.changxiao@dhs.sg>
2014-02-04 17:00:45 +0800

    Update q04_determine_leap_year.py

--- p2/q04_determine_leap_year.py
+++ p2/q04_determine_leap_year.py
@@ -1,10 +1,12 @@
 # q04_determine_leap_year.py
 
-x = int(input("Enter year: ")
+x = int(input("Enter year: "))
 
-if x % 4 == 0 and x % 100 == 0:
+if x % 400 == 0:
     print("Leap")
-elif  x % 4 == 0 and x % 100 != 0:
-    print("Non-Leap")
-elif x % 4 != 0:
-    print("Non-Leap")
+elif x % 100 == 0:
+    print("Non-leap")
+elif x % 4 == 0:
+    print("Leap")
+else:
+    print("Non-leap")
