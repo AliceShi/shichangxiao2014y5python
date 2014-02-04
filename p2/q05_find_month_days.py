76d2e9704de9dc739ae8f62ffd1155151ac8cfb7
AliceShi <shi.changxiao@dhs.sg>
2014-02-04 17:08:43 +0800

    Create q05_find_month_days.py

---
+++ p2/q05_find_month_days.py
@@ -0,0 +1,24 @@
+# q05_find_month_days.py
+
+
+month = int(input("Please enter the month (Jan=1, Dec=12): "))
+year = int(input("Please enter the year: "))
+
+dic = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+
+if year % 400 == 0:
+    leap = 1
+elif year % 100 == 0:
+    leap = 0
+elif year % 4 == 0:
+    leap = 1
+else:
+    leap = 0
+
+if month == 2:
+    days = dic[month] + leap
+else:
+    days = dic[month]
+
+print(days)    
+
