70eb226936f05745af024933e1f0ffa96577cf7f
AliceShi <shi.changxiao@dhs.sg>
2014-02-05 22:00:54 +0800

    Create q12_find_factors.py

---
+++ p2/q12_find_factors.py
@@ -0,0 +1,21 @@
+# q12_find_factors.py
+
+def pFactors(n): 
+        
+        import math 
+        pFact = []
+        check = 2
+        if n == 1: return [1] 
+        for check in range(2, int(math.sqrt(n)+1)): 
+             while n % check == 0: 
+                pFact.append(check) 
+                n /= check 
+        if n > 1: 
+          pFact.append(n) 
+        return pFact
+        
+n = int(input("Enter an integer: "))
+print (pFactors(n))
+
+
+
